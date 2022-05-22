1ee7f238fc90ba903afdfb8f9ebf90bbff498214
Exequiel De Carlini <edecarlini@aconcaguasf.com>
2022-05-22 12:23:16 -0300

    resuelvo las solicitudes del readme y modifico el tsconfig para resolver el noImplicityAny

--- supermarket-list/case-0/src/App.tsx
+++ supermarket-list/case-0/src/App.tsx
@@ -6,23 +6,27 @@ import styles from "./App.module.scss";
 import api from "./api";
 
 function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
     api.list().then(setItems);
   }, []);
 
+  const deleteItem = (id) => {
+    setItems((item) => item.filter((item) => item.id !== id));
+  };
+
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input autoFocus name="text" type="text" />
         <button>Add</button>
       </form>
       <ul>
         {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+          <li key={item.id} className={item.completed ? styles.completed : ""}>
+            {item.text} <button onClick={() => deleteItem(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
